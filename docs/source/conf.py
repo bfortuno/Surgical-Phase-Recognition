667430bda536486dae889b71d56cfb71d8b82945
bfortuno <bfortuno@MMI-1193.>
2025-02-26 17:39:57 +0100

    Fixed Sphinx table formatting issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,6 +22,7 @@ extensions = [
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
+    'python3': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
