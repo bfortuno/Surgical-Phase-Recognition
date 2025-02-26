0ff80e39a1dffdfbb951b003157504a35a82c581
bfortuno <bfortuno@MMI-1193.>
2025-02-26 17:07:58 +0100

    Fixed Sphinx table formatting issue

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Surgical Phase Recognition'
+copyright = '2025, Benjamin I. Fortuno'
+author = 'B.I. Fortuno'
 
 release = '0.1'
 version = '0.1.0'
